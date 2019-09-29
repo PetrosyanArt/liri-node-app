123429f17e18d772d736ee319d0e708cfc598c00
Art Petrosyan <petrosyanart@gmail.com>
2019-09-29 19:21:15 -0400

    added screenshots

--- liri.js
+++ liri.js
@@ -1,178 +1,98 @@
 
 require("dotenv").config();
-
 var fs = require("fs");
-
 var keys = require("./keys.js");
-
 var Spotify = require("node-spotify-api");
-
 var axios = require("axios");
-
 var moment = require('moment');
 
 var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2];
-
-var songName = process.argv[3];
-
-var bitArtist = process.argv[3];
-
-var movie = process.argv[3];
-
+var songName = process.argv.slice(3).join(" ");
+var bitArtist = process.argv.slice(3).join(" ");
+var movie = process.argv.slice(3).join(" ");
 var logStart = "\n] LIRI BOT [\n";
-
 var logEnd = "\n] -******** e n d ********- [\n";
 
 switch (command) {
-
     case "concert-this":
-
         if (bitArtist == null) {
-
             var text = (logStart + command + " :: " + bitArtist + "\n" + "I'm sorry nobody is playing in your neighborhood, please try your search again..." + logEnd);
-
             console.log("I'm sorry nobody is playing in your neighborhood, please try your search again...");
-
             fs.appendFile("log.txt", text, function (err) {
-
                 if (err) {
-
                     console.log(err);
-
                 } else {
-
                     console.log("Liri Log has been updated...");
-
                 }
-
             })
-
         } else {
-
             axios.get("https://rest.bandsintown.com/artists/" + bitArtist + "events/?app_id=codingbootcamp")
-
                 .then(function (response) {
-
                     console.log(bitArtist + " is playing at the...");
-
                     console.log("Name of the venue: " + response.data[0].venue.name);
-
                     console.log("Location: " + response.data[0].venue.city);
-
                     console.log("Date of event: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
-
                     var text = [logStart + command + " :: " + bitArtist + "\n" + "Name of the venue: " + response.data[0].venue.name + "\n" + "Location: " + response.data[0].venue.city + "\n" + "Date of event: " + moment(response.data[0].datetime).format("MM/DD/YYYY") + logEnd];
-
                     fs.appendFile("log.txt", text, function (err) {
-
                         if (err) {
-
                             console.log(err);
-
                         } else {
-
                             console.log("Liri Log has been updated...");
                         }
-
                     });
-
                 },
                     function (error) {
-
                         if (error.response) {
-
                             console.log(error.response.data);
-
                             console.log(error.response.status);
-
                             console.log(error.response.headers);
-
                         } else if (error.request) {
-
                             console.log(error.request);
-
                         } else {
-
                             console.log('Error', error.message);
-
                         }
-
                         console.log(error.config);
                     }
                 )
         }
-
         break;
-
     case "spotify-this-song":
-
         if (songName == null) {
-
             if (command) {
-
                 spotify.request('https://api.spotify.com/v1/tracks/3DYVWvPh3kGwPasp7yjahc')
-
                     .then(function (data) {
-
                         var artists = data.artists[0].name;
-
                         var songTitle = data.name;
-
                         var songUrl = data.preview_url;
-
                         var songAlbum = data.album.name;
-
                         console.log("Artist(s): " + data.artists[0].name);
-
                         console.log("The Song's Name: " + data.name);
-
                         console.log("Spotify Preview Link: " + data.preview_url);
-
                         console.log("Album: " + data.album.name);
-
                         var text = [logStart + process.argv[2] + " :: " + process.argv[3] + "\n" + "Artist(s): " + artists + "\n" + "The Song's Name: " + songTitle + "\n" + "Spotify Preview Link: " + songUrl + "\n" + "Album: " + songAlbum + logEnd];
-
                         fs.appendFile("log.txt", text, function (err) {
-
                             if (err) {
-
                                 console.log(err);
-
                             }
-
                             else {
-
                                 console.log("Liri Log has been updated...");
                             }
-
                         });
-
                     })
-
                     .catch(function (err) {
-
                         console.error('Error occurred: ' + err);
-
                     });
             }
-
         } else {
-
             if (command) {
-
                 spotify.search({
-
                     type: "track",
-
                     query: songName,
-
                     limit: 10
                 },
-
                     function (err, data) {
-
                         if (err) {
 
                             console.log('Error occurred: ' + err);
@@ -406,3 +326,5 @@ switch (command) {
         console.log("I'm so sorry, but your search returned no valuable results...");
 
 } // end switch
+
+
